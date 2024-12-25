a572c1ba61ddf81bf8cbb76f686f87f4971e6ef3
Alexander Urban <lexa79@pm.me>
2024-12-25 23:41:54 +0100

    Page "Exercise" improved (WIP)

--- src/pages/Exercise.jsx
+++ src/pages/Exercise.jsx
@@ -1,51 +1,147 @@
-import React from 'react';
+import { Box, Button, TextField } from '@mui/material';
+import React, { useEffect, useRef, useState } from 'react';
+import * as Icons from '@mui/icons-material';
 
 import { useExerciseContext } from '../context';
 import BaseLayout from '../helpers/BaseLayout';
 
 export default Exercise;
 
-const TEST_LESSONS = [
-    { text: 'Wort 1', translation: 'ord 1', comment: 'dummy translation 1' },
-    { text: 'Wort 2', translation: 'ord 2', comment: 'dummy translation 2' },
-    { text: 'Wort 3', translation: 'ord 3', comment: 'dummy translation 3' },
-    { text: 'Wort 4', translation: 'ord 4', comment: 'dummy translation 4' },
-];
+const STEPS = {
+    INITIAL: 'initial',
+    QUESTION: 'question',
+    ANSWER: 'answer',
+    PENDING: 'pending',
+};
 
 function Exercise() {
-    const context = useExerciseContext();
+    const { getLessonsCount, isLastLesson, getCurrLesson, gotoFirstLesson, gotoNextLesson } = useExerciseContext();
 
-    if (context.hasCurrLesson()) {
-        const lesson = context.getCurrLesson();
+    const [currStep, setCurrStep] = useState(STEPS.INITIAL);
+    const [userInput, setUserInput] = useState('');
+
+    const styles = getSxStyles();
+
+    /** @type {React.RefObject<HTMLButtonElement>} */
+    const primaryButtonRef = useRef(null);
+    /** @type {React.RefObject<HTMLInputElement>} */
+    const focusedInputRef = useRef(null);
+
+    useEffect(() => {
+        if (focusedInputRef.current) {
+            focusedInputRef.current.focus();
+        }
+        if (primaryButtonRef.current) {
+            primaryButtonRef.current.focus();
+        }
+    }, [focusedInputRef.current, primaryButtonRef.current]);
+
+    if (getLessonsCount() === 0 || (currStep === STEPS.PENDING && isLastLesson())) {
+        return <BaseLayout title="Glosförhör">No more words for today.</BaseLayout>;
+    }
+
+    const lesson = getCurrLesson();
+
+    if (!lesson || currStep === STEPS.INITIAL) {
+        const onStart = () => {
+            if (!lesson) {
+                gotoFirstLesson();
+            }
+            setCurrStep(STEPS.QUESTION);
+        };
         return (
-            <BaseLayout title="Glosförhör">
-                <div style={{ display: 'grid', gridTemplateColumns: 'auto auto' }}>
-                    <div>Svenska</div>
-                    <div>Tyska</div>
-                    <div>{lesson.text}</div>
-                    <div>{lesson.translation}</div>
-                    <div>{lesson.comment}</div>
-                    <div />
-                </div>
+            <BaseLayout title="Glosförhör" hasMainContainer>
+                <Button variant="contained" onClick={onStart} ref={primaryButtonRef}>
+                    Let&apos;s go
+                </Button>
             </BaseLayout>
         );
     }
 
-    if (context.getLessonsCount() > 0) {
-        return (
-            <BaseLayout title="Glosförhör">
-                <button type="button" onClick={() => context.gotoFirstLesson()}>
-                    Start
-                </button>
-            </BaseLayout>
-        );
+    switch (currStep) {
+        case STEPS.QUESTION: {
+            const onCheck = () => {
+                setCurrStep(STEPS.ANSWER);
+            };
+            /** @param {React.ChangeEvent<HTMLInputElement} event */
+            const onInput = event => {
+                setUserInput(event.target.value);
+                if (event.target.value === lesson.translation) {
+                    onCheck();
+                }
+            };
+            return (
+                <BaseLayout title="Glosförhör" hasMainContainer>
+                    <Box sx={styles.twoColumns}>
+                        <div>Translate:</div>
+                        <div>{lesson.text}</div>
+                        <div />
+                        <TextField label="Swedish" value={userInput} onChange={onInput} inputRef={focusedInputRef} />
+                        <Button variant="contained" onClick={onCheck}>
+                            Check
+                        </Button>
+                    </Box>
+                </BaseLayout>
+            );
+        }
+        case STEPS.ANSWER: {
+            const onWrong = () => {
+                setCurrStep(STEPS.PENDING);
+                setUserInput('');
+            };
+            const onCorrect = () => {
+                setCurrStep(STEPS.PENDING);
+                setUserInput('');
+            };
+            return (
+                <BaseLayout title="Glosförhör" hasMainContainer>
+                    <Box sx={styles.twoColumns}>
+                        <div>Svenska</div>
+                        <div>Tyska</div>
+                        <div>{lesson.text}</div>
+                        <div>{lesson.translation}</div>
+                        <div>{lesson.comment}</div>
+                        <div />
+                        <Button variant="outlined" startIcon={<Icons.Dangerous />} onClick={onWrong}>
+                            Wrong
+                        </Button>
+                        <Button
+                            variant="contained"
+                            startIcon={<Icons.Verified />}
+                            onClick={onCorrect}
+                            ref={primaryButtonRef}
+                        >
+                            Correct
+                        </Button>
+                    </Box>
+                </BaseLayout>
+            );
+        }
+        case STEPS.PENDING: {
+            const onNext = () => {
+                gotoNextLesson();
+                setCurrStep(STEPS.QUESTION);
+            };
+            return (
+                <BaseLayout title="Glosförhör" hasMainContainer>
+                    <Button variant="contained" onClick={onNext} ref={primaryButtonRef}>
+                        Next word
+                    </Button>
+                </BaseLayout>
+            );
+        }
+        default:
+            throw new Error(`<Exercise/> failed - invalid step ${currStep}`);
     }
+}
 
-    return (
-        <BaseLayout title="Glosförhör">
-            <button type="button" onClick={() => context.setAllLessons(TEST_LESSONS)}>
-                Init
-            </button>
-        </BaseLayout>
-    );
+/** @returns {import('../types').TSxStyles<"twoColumns">} */
+function getSxStyles() {
+    return {
+        twoColumns: {
+            display: 'grid',
+            gridTemplateColumns: 'auto auto',
+            gap: 1,
+        },
+    };
 }
